0ae897d7458530b76237f3fa40d74353be8367c9
Jeremy Gibbs <jeremy.gibbs@noaa.gov>
2026-04-27 13:55:33 -0500

    Added support for MYNN-EDMF and MYNN-SFC

--- src/tools/registry/gen_inc.c
+++ src/tools/registry/gen_inc.c
@@ -1405,8 +1405,8 @@ int parse_var_array(FILE *fd, ezxml_t registry, ezxml_t superStruct, ezxml_t var
 							}
 						}
 
-						fortprintf(fd, "! Define constituent var %s\n", varname);
-						fortprintf(fd, "! My packages are %s\n", varpackages);
+						fprintf(fd, "! Define constituent var %s\n", varname);
+						fprintf(fd, "! My packages are %s\n", varpackages);
 
 						// Parse packages if they are defined
 						sub_spacing[0] = '\0';
